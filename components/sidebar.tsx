3fb51c78c06aa21c88c2fae1c15eb28b8fb9106b
Nyan Htet <nyanhtet@yahoo.com>
2022-05-25 22:13:12 -0700

    sidebar playlist div

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,4 @@
-import NextImage from "next/image";
+import Image from "next/image";
 import NextLink from "next/link";
 
 import {
@@ -19,6 +19,7 @@ import {
   MdPlaylistAdd,
   MdFavorite,
 } from "react-icons/md";
+import { relative } from "path";
 
 const navMenu = [
   {
@@ -60,43 +61,84 @@ const Sidebar = () => {
       paddingX="5px"
       color="gray"
     >
-      <Box width="120px" marginBottom="20px" paddingX="20px">
-        <NextImage src="/test.svg" height={100} width={100} />
+      <Box paddingY="20px" height="100%">
+        <Box width="120px" marginBottom="20px" paddingX="20px">
+          <NextLink href="/" passHref>
+            <LinkOverlay>
+              <Image src="/basscloud.svg" height={100} width={200} />
+            </LinkOverlay>
+          </NextLink>
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {navMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box marginTop="20px">
+          <List spacing={2}>
+            {musicMenu.map((icon) => (
+              <ListItem paddingX="20px" fontSize="16px" key={icon.name}>
+                <LinkBox>
+                  <NextLink href={icon.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={icon.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {icon.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Divider color="gray.800" marginTop="20px" />
+        <Box
+          height="66%"
+          overflowY="auto"
+          paddingY="20px"
+          sx={{
+            "&::-webkit-scrollbar": {
+              width: "16px",
+              borderRadius: "8px",
+              backgroundColor: `rgba(226, 226, 226, 0.05)`,
+            },
+            "&::-webkit-scrollbar-thumb": {
+              borderRadius: "8px",
+              backgroundColor: `rgba(128, 128, 128, 0.243)`,
+            },
+          }}
+        >
+          <List spacing="2">
+            {playlists.map((playlist) => (
+              <ListItem paddingX="20px" key={playlist}>
+                <LinkBox>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{playlist}</LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Box>
-      <Box paddingY="20px">
-        <List spacing={2}>
-          {navMenu.map((menu) => (
-            <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
-              <LinkBox>
-                <NextLink href={menu.route} passHref>
-                  <LinkOverlay>
-                    <ListIcon as={menu.icon} color="white" marginRight="20px" />
-                    {menu.name}
-                  </LinkOverlay>
-                </NextLink>
-              </LinkBox>
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-      <Box marginTop="20px">
-        <List spacing={2}>
-          {musicMenu.map((icon) => (
-            <ListItem paddingX="20px" fontSize="16px" key={icon.name}>
-              <LinkBox>
-                <NextLink href={icon.route} passHref>
-                  <LinkOverlay>
-                    <ListIcon as={icon.icon} color="white" marginRight="20px" />
-                    {icon.name}
-                  </LinkOverlay>
-                </NextLink>
-              </LinkBox>
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-      <Divider color="gray.800" marginTop="20px" />
-      <Box height="66%" overflow="auto" paddingY="20px"></Box>
     </Box>
   );
 };
